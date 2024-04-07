7787165a4c59fb747d8dd4dd875d3518a8961a5a
Joy <mbuvimumbuambua99@gmail.com>
2024-04-07 13:57:20 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,120 +1,81 @@
-/ Your code here
-let url = "http://localhost:3000/films"
-//console.log(document.getElementById('films'))
+let allmovies=[]
+
+function getMovies() {
+  
+
+const requestOptions = {
+  method: "GET",
+  redirect: "follow"
+};
+
+fetch("http://localhost:3000/films", requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+  //console.log(result);
+  ListMovies(result)
+  allmovies=result
+})
+  .catch((error) => console.error(error));
+}
+
+function ListMovies(movies) {
+  const movieList= document.getElementById('films');
+  let html ="";
 
 
-document.getElementsByClassName('film item')[0].remove()
-fetchMovies(url)
+  for (let i = 0; i<movies.length; i++) {
+    let movie = movies[i];
+    html= html + `<li class="film item" onClick="clickedMovie(${i})">${movie.title}</li>`;
+  }
+
+  movieList.innerHTML = html;
+}
+ getMovies();
+
+ function clickedMovie(id) {
+  let poster =document.getElementById('poster')
+  let clickedMovie= allmovies[id]
+  poster.src=clickedMovie.poster;
+  poster.alt = clickedMovie.title;
+  movieinformation(clickedMovie.id)
+ };
 
 
-//Create fetch function
-function fetchMovies(url){
-    fetch(url)
-    .then(response => response.json())
-    .then(movies => {
-        movies.forEach(movie => {
-            displayMovie(movie)
-           // console.log(movie)
-        });
+ function movieinformation(id) {
+  let movietitle= document.getElementById("title")
+  let runtime=document.getElementById("runtime")
+  let filminfo=document.getElementById("film-info")
+  let ticketnumber=document.getElementById("ticket-num")
+   let showtime=document.getElementById("showtime" )
+  let button= document.getElementById("buy-ticket" )
+   const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
+  
+  fetch(`http://localhost:3000/films/${id}`, requestOptions)
+
+    .then((response) => response.json())
+    .then((result) =>{
+      console.log(result)
+    movietitle.innerText = result.title;
+    runtime.innerText= `${result.runtime} minutes`
+    filminfo.innerText=result.description
+    showtime.innerText= result.showtime
+    ticketnumber.innerText=`remaining  tickets ${result.capacity - result.tickets_sold}`
     })
-}
+    .catch((error) => console.error(error));
 
-// function reduceavailabletickets('http://localhost:3000/films/${movi.id}'){
-//     method: "PATCH",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//         "available_tickets": 0
-//     })
-//     .then((response) => response.json()) // Parse response as JSON
-//     .then((data) => console.log("Data updated successfully:", data))
-//     .catch((error) => console.error("Error updating data:", error));
+ }
 
-// }
-
-
-
-
-
-let movieTicketsSold=0;
-let movieCapacity=0;
-let pointsf;
-
-
-//to display list of movies
-function displayMovie(movie) {
-    let movi = document.createElement("li");
-    movi.textContent = movie.title;
-    movi.runtime = movie.runtime; 
-    movi.capacity = movie.capacity; 
-    movi.description = movie.description; 
-    movi.showtime = movie.showtime; 
-    movi.ticketsSold = movie.tickets_sold; 
-    movi.poster = movie.poster; 
-    movi.className = "filmitem";
-    movi.id = movie.id;
-    document.getElementById("films").appendChild(movi);
-    movi.addEventListener("click", updatetherest);
-}
-
-//to add an event listener to every movie
-function updatetherest(event) {
-    pointsf=event.target
-    //console.log(pointsf.capacity)
-    const movieTitle = event.target.textContent;
-    const movieruntime = event.target.runtime; 
-    movieCapacity = event.target.capacity; 
-    const movieDescription = event.target.description; 
-    const movieShowtime = event.target.showtime; 
-    movieTicketsSold = event.target.ticketsSold; 
-    const moviePoster = event.target.poster; 
+function buttonclick() {
+  let result=Currentmovie
+  let remainingTickets= Currentmovie-SpeechRecognitionResultList.ticket_sold
+   if (remainingTickets>0) {
+    makeSale(result)
+   }else{
+    ticket.innerText="Sorry Sold Out!"
+   }
     
-    //alert("You clicked the movie with title: " + movieTitle);
-    document.getElementById("title").textContent = ("["+movieTitle+"]")
-    document.getElementById("runtime").textContent = "["+movieruntime+"] minutes"
-    document.getElementById("film-info").textContent = movieDescription
-    document.getElementById("showtime").textContent = ("["+movieShowtime+"]")
-    document.getElementById("ticket-num").textContent = ("["+(movieCapacity-movieTicketsSold)+"]")
-    document.getElementById("poster").src = moviePoster
-}
+   }
 
-//to add an event listener to the butto
-document.getElementById("buy-ticket").addEventListener("click", updatebtn)
-function updatebtn(){
-
-    alert("Please note that on every click you are updating the server and its not reversible. so ukifanya test keep track ya movie umeselect")
-    let ticketssoldsupdate={
-        "id":pointsf.id,
-        "tickets_sold":movieTicketsSold+1
-    }
-    //console.log(pointsf.id)
-    console.log(ticketssoldsupdate)
-
-    //to update the server
-    fetch(url + '/' + pointsf.id, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(ticketssoldsupdate)
-      })
-        .then(response => response.json()) 
-        .then(data => console.log("Tickets updated successfully:", data))
-        .catch(error => console.error("Error updating tickets:", error));
-
-
-
-
-    //to update the available tickets for the movie after sale
-    let availableTickets =movieCapacity-movieTicketsSold
-    if(availableTickets>1){
-        movieTicketsSold +=1;
-        availableTickets -=1;
-
-        //console.log("local ticketsold"+pointsf.ticketsSold)
-
-       // this only for the button
-        document.getElementById("ticket-num").textContent = "[" + availableTickets + "]";}
-    else ((document.getElementById("ticket-num").textContent = "[" + "SOLD OUT!!" + "]")
-          (console.log("SOLD OUT!!")))}
